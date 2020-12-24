af9322dafe420b9f9c78bd19ed103fc76237bdd4
Daniel151505 <daniel150520011@gmail.com>
2020-12-23 20:26:35 -0500

    Creación de Identity y de Pages

---
+++ AplicacionWebProducto/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using AplicacionWebProducto.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AplicacionWebProducto.Areas.Identity.IdentityHostingStartup))]
+namespace AplicacionWebProducto.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
