d58f22af9db473543540e5ce839cab9ba7a6093c
Daniel151505 <daniel150520011@gmail.com>
2020-12-24 23:21:47 -0500

    Agregando metodos en el modelo Contacto

---
+++ AplicacionWebProducto/Models/Contacto.cs
@@ -0,0 +1,52 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AplicacionWebProducto.Models
+{
+    [Table("t_contacto")]
+    public class Contacto
+    { 
+    
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set;}
+
+        [Required(ErrorMessage="Debe escribir los nombres")]
+        [Column("name")]
+        [Display (Name="Nombre")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage="Debe escribir los apellidos")]
+         
+
+        [Display (Name="Apellidos")]
+        [Column("lastname")]
+        public string LastName { get; set; }
+
+        [Display (Name="Correo")]  
+         [Column("email")]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Display (Name="Telefono")]
+        [Column("number")]
+        public int Number { get; set; }
+
+        [Display (Name="Curso")]
+        [Column("course")]
+        public string Course { get; set; }
+
+        [Display (Name="Titulo")]
+         [Column("title")]
+        public string Title { get; set; }
+
+        [Display (Name="Mensaje")]
+         [Column("message")]
+        public string Message { get; set; }
+
+        [NotMapped]
+        public string Response {get;set;}
+
+    }
+}
